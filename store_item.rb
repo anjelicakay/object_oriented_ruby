a0892e08f97a70f68d4c44c3bd3ee4893afdb41a
anjelicakay <aktowner@gmail.com>
2019-01-08 18:45:51 -0600

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+bag_1 = {:brand => "Coach", :color => "black", :price => 150}
+bag_2 = {:brand => "Michael Kors", :color => "brown", :price => 170}
+bag_3 = {brand: "Kate Spade", color: "red", price: 140}
+
+
+puts "We have a #{ bag_1[:brand] } purse in #{ bag_1[:color] } for #{ bag_1[:price] }"
+puts "We have a #{ bag_2[:brand] } purse in #{ bag_2[:color] } for #{ bag_2[:price] }"
+puts "We have a #{ bag_3[:brand] } purse in #{ bag_3[:color] } for #{ bag_3[:price] }"
